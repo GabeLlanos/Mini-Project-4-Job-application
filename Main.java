5ee2e5a7c32a41bb0ad7de54a1bb6b5eff72c7b7
GabeLlanos <gabriel.llanos@patelhs.org>
2021-01-29 13:32:43 +0000

    I added questions for the actual interview

--- Main.java
+++ Main.java
@@ -12,21 +12,10 @@ class Main {
     System.out.println("Have you worked before true/false");
     boolean timeWorked = scan.nextBoolean();
     System.out.println("Was your time at work above 2 months? true/false");
-    boolean expCheck = scan.nextInt();
+    boolean expCheck = scan.nextBoolean();
     System.out.println("What is your age");
     int ageCheck = scan.nextInt();
    
-  
-     int ageCheck = true;
-     if (ageCheck)
-     {
-         System.out.println("Next.");
-     }
-     else
-     {
-       System.out.println("Sorry re apply next time!");
-     }
-
     
    }
 }
